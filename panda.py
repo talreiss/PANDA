d1251f8b849a103ee2921fd8b75f71e944c99f59
Tal Reiss <tal.reiss@gmail.com>
2021-04-08 22:04:04 +0300

    updated panda.py

--- panda.py
+++ panda.py
@@ -6,22 +6,14 @@ import argparse
 from losses import CompactnessLoss, EWCLoss
 import utils
 from copy import deepcopy
+from tqdm import tqdm
 
 def train_model(model, train_loader, test_loader, device, args, ewc_loss):
     model.eval()
-    auc = get_score(model, device, train_loader, test_loader)
+    auc, feature_space = get_score(model, device, train_loader, test_loader)
     print('Epoch: {}, AUROC is: {}'.format(0, auc))
     optimizer = optim.SGD(model.parameters(), lr=args.lr, weight_decay=0.00005, momentum=0.9)
-    features_space = []
-    with torch.no_grad():
-        for i, (imgs, _) in enumerate(train_loader):
-            images = imgs.to(device)
-            _, features = model(images)
-            batch_size = features.shape[0]
-            for j in range(batch_size):
-                features_space.append(features[j].detach().cpu().numpy())
-    train_set = np.array(features_space)
-    center = torch.FloatTensor(train_set).mean(dim=0)
+    center = torch.FloatTensor(feature_space).mean(dim=0)
     criterion = CompactnessLoss(center.to(device))
     for epoch in range(args.epochs):
         running_loss = run_epoch(model, train_loader, optimizer, criterion, device, args.ewc, ewc_loss)
@@ -57,34 +49,27 @@ def run_epoch(model, train_loader, optimizer, criterion, device, ewc, ewc_loss):
 
 
 def get_score(model, device, train_loader, test_loader):
-    features_space = []
+    train_feature_space = []
     with torch.no_grad():
-        for i, (imgs, _) in enumerate(train_loader):
+        for (imgs, _) in tqdm(train_loader, desc='Train set feature extracting'):
             imgs = imgs.to(device)
             _, features = model(imgs)
-            batch_size = features.shape[0]
-            for j in range(batch_size):
-                features_space.append(features[j].detach().cpu().numpy())
-    train_set = np.array(features_space)
-    anom_labels = []
-    features_space = []
+            train_feature_space.append(features)
+        train_feature_space = torch.cat(train_feature_space, dim=0).contiguous().cpu().numpy()
+    test_feature_space = []
     with torch.no_grad():
-        for i, (imgs, labels) in enumerate(test_loader):
-            imgs, labels = imgs.to(device), labels.numpy()
+        for (imgs, _) in tqdm(test_loader, desc='Test set feature extracting'):
+            imgs = imgs.to(device)
             _, features = model(imgs)
-            batch_size = features.shape[0]
-            for j in range(batch_size):
-                features_space.append(features[j].detach().cpu().numpy())
-                anom_labels.append(labels[j])
-    test_set = np.array(features_space)
-    test_labels = np.array(anom_labels)
+            test_feature_space.append(features)
+        test_feature_space = torch.cat(test_feature_space, dim=0).contiguous().cpu().numpy()
+        test_labels = test_loader.dataset.targets
 
-    distances = utils.knn_score(train_set, test_set)
+    distances = utils.knn_score(train_feature_space, test_feature_space)
 
     auc = roc_auc_score(test_labels, distances)
 
-    return auc
-
+    return auc, train_feature_space
 
 def main(args):
     print('Dataset: {}, Normal Label: {}, LR: {}'.format(args.dataset, args.label, args.lr))
@@ -106,7 +91,6 @@ def main(args):
     utils.freeze_parameters(model)
     train_loader, test_loader = utils.get_loaders(dataset=args.dataset, label_class=args.label, batch_size=args.batch_size)
     train_model(model, train_loader, test_loader, device, args, ewc_loss)
-
 
 
 if __name__ == "__main__":
